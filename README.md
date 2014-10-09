3b7b029ec0c32d7ac6c9f979c06d37ae8691a299
Suissa <jnascimento@gmail.com>
2014-10-08 22:34:15 -0300

    Explicaçnao no README

--- README.md
+++ README.md
@@ -15,3 +15,103 @@ Irá me listar todas as cervejas
 
 Irá criar uma cerveja nova
 
+
+###Refatorando
+
+Vamos criar uma função de callback na rota para que ela responda da melhor forma, porém para isso precisaremos passar o callback como parametro das nossas funções do Controller.
+
+```
+//callback
+var callback = function(err, data, res){
+  if (err){
+    msg = 'Erro: ' + err;
+    console.log('Erro: ', err);
+  }
+  else{
+    msg = data;
+    console.log('Cerveja Inserida: ', data);
+  }
+  res.json(msg);
+};
+
+router.get('/', function(req, res) {
+  Controller.retrieve(req, res, callback);
+});
+
+// passando a variável :id para nossa rota
+router.get('/:id', function(req, res) {
+  Controller.show(req, res, callback);
+});
+
+// POST /api/beers
+router.post('/', function(req, res) {
+  Controller.create(req, res, callback);
+});
+
+// Altero a cerveja
+router.put('/:id', function(req, res) {
+  Controller.update(req, res, callback);
+});
+
+// Deleto a cerveja
+router.delete('/:id', function(req, res) {
+  Controller.delete(req, res, callback);
+});
+```
+
+Então no nosso Controller precisamos receber esse callback e executá-lo:
+```
+var _beer = {
+  create: function(req, res, callback) {
+    // recebendo os dados do POST
+    var dados = req.body,
+      model = new Beer(dados),
+      msg = '';
+
+    model.save(function (err, data) {
+      callback(err, data, res);
+    });
+
+  },
+  retrieve: function(req, res, callback) {
+    Beer.find({}, function (err, data) {
+      callback(err, data, res);
+    });
+
+  },
+  show: function(req, res, callback) {
+    var query = {_id: req.params.id};
+    Beer.findOne(query, function (err, data) {
+      callback(err, data, res);
+    });
+
+  },
+  update: function(req, res, callback) {
+    var query = {_id: req.params.id},
+      mod = req.body,
+      optional = {
+        upsert: false,
+        multi: true
+      };
+
+    Beer.update(query, mod, function (err, data) {
+      callback(err, data, res);
+    });
+
+  },
+  delete: function(req, res, callback) {
+    var query = {_id: req.params.id};
+
+    Beer.remove(query, function(err, data) {
+      callback(err, data, res);
+    });
+
+  },
+};
+```
+
+
+
+
+
+
--- controllers/beers.js
+++ controllers/beers.js
@@ -1,87 +1,48 @@
 var Beer = require('../models/beer');
 
 var _beer = {
-  create: function(req, res) {
-
+  create: function(req, res, callback) {
     // recebendo os dados do POST
-    var dados = req.body;
-
-    var model = new Beer(dados),
+    var dados = req.body,
+      model = new Beer(dados),
       msg = '';
 
     model.save(function (err, data) {
-      if (err){
-        msg = 'Erro: ' + err;
-        console.log('Erro: ', err);
-      }
-      else{
-        msg = data;
-        console.log('Cerveja Inserida: ', data);
-      }
-      res.json(msg);
+      callback(err, data, res);
     });
 
   },
-  retrieve: function(req, res) {
+  retrieve: function(req, res, callback) {
     Beer.find({}, function (err, data) {
-      if (err){
-        msg = 'Erro: ' + err;
-        console.log('Erro: ', err);
-      }else{
-        msg = data;
-        console.log('Listagem: ', data);
-      }
-      res.json(msg);
+      callback(err, data, res);
     });
 
   },
-  show: function(req, res) {
+  show: function(req, res, callback) {
     var query = {_id: req.params.id};
     Beer.findOne(query, function (err, data) {
-      if (err){
-        msg = 'Erro: ' + err;
-        console.log('Erro: ', err);
-      }else{
-        msg = data;
-        console.log('Listagem: ', data);
-      }
-      res.json(msg);
+      callback(err, data, res);
     });
 
   },
-  update: function(req, res) {
-    var query = {_id: req.params.id};
-    var mod = req.body;
-    var optional = {
+  update: function(req, res, callback) {
+    var query = {_id: req.params.id},
+      mod = req.body,
+      optional = {
         upsert: false,
         multi: true
       };
 
     Beer.update(query, mod, function (err, data) {
-      if (err){
-        msg = 'Erro: ' + err;
-        console.log('Erro: ', err);
-      }
-      else{
-        msg = data;
-        console.log('Cervejas atualizadas com sucesso', data);
-      }
-      res.json(msg);
+      callback(err, data, res);
     });
 
   },
-  delete: function(req, res) {
+  delete: function(req, res, callback) {
     var query = {_id: req.params.id};
 
     Beer.remove(query, function(err, data) {
-      if(err) {
-        msg = 'Erro: ' + err;
-        console.log(err);
-      } else {
-        msg = data;
-        console.log('Cerveja deletada com sucesso, quantidade: ', data);
-      }
-      res.json(msg);
+      callback(err, data, res);
     });
 
   },
